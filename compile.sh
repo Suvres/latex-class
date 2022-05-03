042923223b7a780c4e7860fec26cb58b012d4ce1
Suvres <bartosz.blyszcz@gmail.com>
2022-05-03 15:19:22 +0200

    polska klasa + skrypt kompilacji

---
+++ compile.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+mkdir -p _tex
+
+
+if ! [ -d _tex ]; then
+	echo "Brak katalogu \`_tex' !"
+	exit 1
+fi
+
+
+echo -ne "\033]0;▶LaTEX compilation ("$PWD")\007"
+
+for x in *.tex
+do
+	if head -n 1 "$x" | grep -q "documentclass"
+	then
+		main=$x
+	fi
+	cat $x | sed 's/ [ ]*/ /g;s/ \([a-zA-Z0-9]\) / \1~ /g' > _tex/$x
+	
+done
+
+if ! [ -e "$main" ]
+then
+	echo -e "Brak pliku \`*.tex' !\033[0;37m"
+	exit 2
+fi
+
+cp ./suvresdpl.cls ./_tex/
+cd _tex;
+ln -s ../images images
+
+
+pdflatex $main; 
+pdflatex $main;
+pdflatex $main;
+
+cp main.pdf ../main.pdf
+cd ..
+
+rm -rf _tex
